349716c21a05cb9220c27d9be16f57b0ac420152
Samandar Turdiev <firsturdiev@gmail.com>
2021-10-19 15:42:50 +0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
+import './bootstrap.min.css';
+import movies from './movies';
+import Card from './Card';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ul className="search-results row list-unstyled">
+    {
+      movies.slice(0, 50).map(movie => <Card title={movie.title} year={movie.year} imdbRating={movie.imdbRating} smallPoster={movie.smallPoster}></Card>)
+    }
+  </ul>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
